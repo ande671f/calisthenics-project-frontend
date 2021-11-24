fb4d77bf63e823dfc5b62cb8c9fdafb2a61a9728
Anders Biller Due <ad@lait.dk>
2021-11-24 09:27:30 +0100

    Sidetype: Forside - Om os

--- calisthenics-fronted/src/components/about/About.vue
+++ calisthenics-fronted/src/components/about/About.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="about row">
-        <div class="col-4 offset-1">
-            <h1 v-if="aboutTitle != null">
-                {{aboutTitle}}
-            </h1>
-
-             <p v-if="about != null">
-                 {{about}}
-             </p>
-        </div>
-
-        <div class="col-4 offset-1">
-             <img class="about__image" src="../../assets/images/about-image.jpg" alt="About image">
-        </div>
-       
-
-    </div>
+	<div class="row">
+		<div class="about col-10 offset-1">
+			<div class="row">
+				<div class="about__content col-5 offset-1">
+					<h1 v-if="aboutTitle != null">
+						{{ aboutTitle }}
+					</h1>
+					<p v-if="about != null">
+						{{ about }}
+					</p>
+				</div>
+				<div class="about__image col-5">
+					<img src="../../assets/images/about-us-image.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script src="./About.ts"></script>
